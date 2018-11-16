64c679bbbca462080193276dab35ee1192dd3014
anonymous <anonymous>
2018-11-15 21:18:49 -0500

    When I tested state before removing Redux, I noticed that running the app with state listed would throw warnings. I thought this may have been a conflict with the way Redux was set up in the starter code, but the problem persisted after my overhaul. I found a solution online by installing a plugin called transform-class-properties that transforms static class properties so that I can use the syntax I am used to uing for React.

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Main extends Component {
+
+  state = {
+    
+  }
+
   render() {
     return (
       <div>
--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const NavWrapper = styled.div`
+display: flex;
+justify-content: space-around;
+align-items: center;
+margin: 8vh;
+`
 
 export default class Nav extends Component {
   render() {
     return (
-      <div>
-        NAV
-      </div>
+      <NavWrapper>
+        <div>Nav Item 1</div>
+        <div>Nav Item 2</div>
+      </NavWrapper>
     )
   }
 }
