ee3b4772a91f62e4248f890205e2548554d53138
anonymous <anonymous>
2018-11-15 19:24:27 -0500

    Added a show one id route in the people controller

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -2,10 +2,8 @@ class Api::PeopleController < ApplicationController
     include HTTParty
 
     def index
-        puts ENV["secret_key"]
-        puts "Banana"
         @people = HTTParty.get(
-            "https://api.salesloft.com/v2/people.json?", 
+            "https://api.salesloft.com/v2/people.json", 
             headers: { 
                 "Authorization" => "Bearer #{ENV["SECRET_KEY"]}" }
             )
@@ -13,6 +11,13 @@ class Api::PeopleController < ApplicationController
     end
 
     def show
+        @id
+        @people = HTTParty.get(
+            "https://api.salesloft.com/v2/people/#{params[:id]}.json",
+            headers: { 
+                "Authorization" => "Bearer #{ENV["SECRET_KEY"]}" }
+        )
+        render json: @people
     end
 
     def person_params
