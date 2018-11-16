4130ffb7a527997abe7d41ca334e432268ae87a6
anonymous <anonymous>
2018-11-15 20:54:53 -0500

    Removed most redux components and references and recreated App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          hello world
+      </div>
+      
+    )
+  }
+}
+
+export default App;
